3d5d775d62b59ad65f7cc0b7ef53b8a91172f470
a.starchan <a.starchan@crpo.su>
2025-02-01 18:21:37 +0300

    added api routes

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'v1', 'middleware' => 'api'], function () {
+    Route::group([
+        'prefix' => 'auth',
+    ], function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('me', [AuthController::class, 'me']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+    });
+
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/', [UserController::class, 'get']);
+        Route::get('{id}', [UserController::class, 'getById']);
+        Route::post('register', [UserController::class, 'register']);
+        Route::put('{id}', [UserController::class, 'update']);
+        Route::delete('{id}', [UserController::class, 'destroy']);
+    });
+
+    Route::group(['prefix' => 'article'], function () {
+        Route::get('/', [ArticleController::class, 'get']);
+        Route::get('{id}', [ArticleController::class, 'getById']);
+        Route::post('/', [ArticleController::class, 'create']);
+        Route::put('{id}', [ArticleController::class, 'update']);
+        Route::delete('{id}', [ArticleController::class, 'destroy']);
+    });
+
+});
